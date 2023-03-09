886629222a162ae724048ace2a1f038203de903f
BOSTONE069 <bostoneochieng69@gmail.com>
2023-03-09 12:22:08 -0500

    Luo medical website

---
+++ luomedicine_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# default: "Django Administration"
+admin.site.site_header = 'LUO MEDICINE'
+# default: "Site administration"
+admin.site.index_title = 'LUO MEDIC'
+# default: "Django site admin"
+admin.site.site_title = 'HTML title from administration'
---
+++ luomedicine_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    """
+    It takes a request object and returns a response object
+
+    :param request: This is the request object that is sent to the view
+    :return: The index.html file is being returned.
+    """
+    return render(request, "luomedicine/index.html")
