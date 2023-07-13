a8b272f8a9594c1ed6010ff1936db0661196d571
rhemney <bostoneochieng69@gmail.com>
2023-07-12 21:06:24 -0700

    Created a model for the medicinal plants

--- luomedicine_app/models.py
+++ luomedicine_app/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class MedicinalPlant(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    prescription = models.TextField()
+    image = models.ImageField(upload_to='medicinal_plant_images/')
 
+    def __str__(self):
+        return self.title
